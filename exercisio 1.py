e8b8e9a31e39fe6d8afbdbd733c1e71070c85015
jose-rodrigo8 <jrodrigobeleza@icloud.com>
2025-11-11 19:01:27 +0000

    Add files via upload

---
+++ exercisio 1.py
@@ -0,0 +1,8 @@
+lista=("Letónia","San Marino","Kosovo","Andorra","Macedónia do Norte")
+while True:
+   paises=input("Qual é o país ")
+
+   if paises in lista:
+      print("Parabens ganhaste! ")
+   else:
+      print("Perdeste kkkkkwkwkwkwkwwkwkwww!")
